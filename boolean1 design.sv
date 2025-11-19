fd7cb5e73de28b31381e5ce98ebe0b49b80b12ee
bhavishya188 <bhavishyaa16@gmail.com>
2025-11-19 18:45:57 +0000

    commit

--- boolean1 design.sv
+++
@@ -1,7 +0,0 @@
-`timescale 1ns / 1ps
-module boolean_expression(A, B, C, Y);
-    input  A, B, C;  
-    output Y;
-  
-    assign Y = (A & B) | (~C);
-endmodule
--- xordesign.v
+++
@@ -1,7 +0,0 @@
-module xor_gate (
-    input wire A,
-    input wire B,
-    output wire Y
-);
-    assign Y = A ^ B;   
-endmodule
